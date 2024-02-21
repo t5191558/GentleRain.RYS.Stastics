6e380d825bf4792d21cc4d28e92e308a926644a1
tianlei <tianlei@schmidt-isoft.com>
2024-02-21 17:13:22 +0800

    加入Serilog日志

--- GentleRain.RYS.Stastics/MounthRevenue/Program.cs
+++ GentleRain.RYS.Stastics/MounthRevenue/Program.cs
@@ -1,3 +1,7 @@
+﻿using Serilog;
+using Serilog.Sinks.File;
+using System.Configuration;
+
 namespace MounthRevenue
 {
     internal static class Program
@@ -8,10 +12,35 @@ namespace MounthRevenue
         [STAThread]
         static void Main()
         {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(ConfigurationManager.AppSettings["logfile"]?.ToString() ?? "log/log.txt",rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+            Log.Information("程序启动");
+
+            // 添加全局异常处理
+            Application.ThreadException += new ThreadExceptionEventHandler(GlobalThreadExceptionHandler);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalUnhandledExceptionHandler);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+        }
+
+        private static void GlobalThreadExceptionHandler(object sender, ThreadExceptionEventArgs e)
+        {
+            Log.Error(e.Exception, e.Exception.Message);
+            MessageBox.Show(e.Exception.Message, "程序发生错误,请联系开发者处理", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            Log.Error(ex, ex.Message);
+            MessageBox.Show(ex.Message, "程序发生错误了,请联系开发者处理", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
