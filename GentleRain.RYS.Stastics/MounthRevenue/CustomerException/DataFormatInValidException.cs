3cd7837de15bd87e63df5108f8e210d8f11f4df2
tianlei <tianlei@schmidt-isoft.com>
2024-02-21 17:28:02 +0800

    开始做一些优化

---
+++ GentleRain.RYS.Stastics/MounthRevenue/CustomerException/DataFormatInValidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthRevenue
+{
+    public class DataFormatInValidException : Exception 
+    {
+        public DataFormatInValidException(string dataName,string reason) : base($"{dataName}数据格式错误:{reason}") { }
+    }
+}
--- GentleRain.RYS.Stastics/MounthRevenue/Repository/BonusEntity.cs
+++ GentleRain.RYS.Stastics/MounthRevenue/Repository/BonusEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,23 @@ namespace MonthRevenue.Repository
     public class BonusEntity
     {
         public int Id { get; set; }
-        [DisplayName("业绩范围开始(包含)")]
+        [DisplayName("业绩范围低(包含)")]
+        [Range(0,100000000)]
         public decimal Low { get; set; }
-        [DisplayName("业绩范围结束(不包含)")]
-        public decimal High { get; set; }
+        [DisplayName("业绩范围高(不包含)")]
+        [Range(0,100000000)]
+        public decimal High { 
+            get { return high; } 
+            set { 
+                    if(value <= Low)
+                        throw new DataFormatInValidException("业绩范围高", "必须大于业绩范围低");
+                    high = value;
+                } 
+        }
         [DisplayName("提成比率")]
+        [Range(0,1,MaximumIsExclusive =true)]
         public decimal Rate { get; set; }
+
+        private decimal high { get; set; }
     }
 }
