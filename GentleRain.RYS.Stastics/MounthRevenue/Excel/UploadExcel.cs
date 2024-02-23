09b5aca0f9551b92e70db126a253f76de71fb2f3
tianlei <tianlei@schmidt-isoft.com>
2024-02-23 16:38:24 +0800

    有一点思路

--- GentleRain.RYS.Stastics/MounthRevenue/Excel/UploadExcel.cs
+++ GentleRain.RYS.Stastics/MounthRevenue/Excel/UploadExcel.cs
@@ -1,6 +1,8 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using MonthRevenue.Repository;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -62,24 +64,74 @@ namespace MonthRevenue
         /// </summary>
         /// <param name="worksheet">当前sheet表</param>        
         /// <returns></returns>
-        private static Dictionary<string, List<string>> ReadData(IXLWorksheet worksheet)
+        private static Dictionary<string, Dictionary<string,decimal>> ReadData(IXLWorksheet worksheet)
         {
-            Dictionary<string, List<string>> result = new Dictionary<string, List<string>>();
+            Dictionary<string, Dictionary<string,decimal>> result = new Dictionary<string, Dictionary<string,decimal>>();
             int lastRow = worksheet.LastRowUsed().RowNumber();
             for(int row = 0; row < lastRow; row++)
             {
+                //有效区域
                 if (IsValidArea(worksheet.Cell(row, 1)))
                 {
-                    int column = 1;
-                    IXLCell cell = worksheet.Cell(row, column);
-                    while (HasColumn(cell))
+                    var readedValues = ReadArea(worksheet,row);
+                    row = readedValues.Item1;
+                    foreach(var line in readedValues.Item3)
                     {
-                        string key = GetColumn(cell);
-                        cell = worksheet.Cell(row, ++column);
+                        foreach(var key in readedValues.Item2)
+                        {
+                            int index = readedValues.Item2.IndexOf(key);
+                            //行首
+                            if(index == 0)
+                            {
+                                if (!result.ContainsKey(line[index]))
+                                {
+                                    result.Add(line[index], new Dictionary<string, decimal>());
+                                }
+                            }
+                            else
+                            {
+                                if (result[line[0]].ContainsKey(key))
+                                {
+                                    result[line[0]][key] = result[line[0]][key] + decimal.Parse(line[index]);
+                                }
+                                else
+                                {
+                                    result[line[0]][key] = decimal.Parse(line[index]);
+                                }
+                            }
+                        }
                     }
                 }
             }
             return result;
+        }
+
+        private static Tuple<int, List<string>, List<List<string>>> ReadArea(IXLWorksheet worksheet,int row)
+        {
+            var heads = ReadHead(worksheet,row);
+            var values = ReadValue();
+            return new Tuple<int, List<string>, List<List<string>>>(row, heads, values);
+        }
+
+        private static List<string> ReadHead(IXLWorksheet worksheet,int row)
+        {
+            //读取列头
+            if (IsCurrentRowIsColumnRow(worksheet, row))
+            {
+                int column = 2;
+                IXLCell cell = worksheet.Cell(row, column);
+                while (HasColumn(cell))
+                {
+                    string key = GetColumn(cell);
+                    cell = worksheet.Cell(row, ++column);
+                }
+            }
+            return new List<string>();
+        }
+
+        private static List<List<string>> ReadValue()
+        {
+            return new List<List<string>>();
         }
 
         private static bool IsValidArea(IXLCell cell)
@@ -96,6 +148,12 @@ namespace MonthRevenue
         {
             return cell.GetString();
         }
+
+        private static bool IsCurrentRowIsColumnRow(IXLWorksheet worksheet ,int row)
+        {
+            return worksheet.Cell(row,1).GetString().Equals("姓名", StringComparison.OrdinalIgnoreCase);
+        }
+
 
     }
 }
